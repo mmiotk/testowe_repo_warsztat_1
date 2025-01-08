3239fc316aba37c003d97b3d2ec1828d30ae2ac8
Mateusz Miotk <mmiotk@inf.ug.edu.pl>
2025-01-08 08:50:07 +0100

    add sum_numbers function

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def div_numbers(a,b):
   return a/b
 
+def sum_numbers(a,b):
+  return a+b
+
 def print_msg(msg):
   print(msg)
 
 
 print(div_numbers(4,2))
 print_msg("Hello world")
+print(sum_numbers(4,2))
